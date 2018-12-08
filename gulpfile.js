3f66a4c5ed28a39d6343671c0789e202bb69a029
Jocelyn Belden <jocebelden@gmail.com>
2018-12-08 07:13:16 -1000

    handle errors in 'watch' task

--- gulpfile.js
+++ gulpfile.js
@@ -17,25 +17,32 @@ if (process.env.CI) {
   };
 }
 
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
 gulp.task('lint', function(done) {
   return gulp.src(paths.lint)
     .pipe(plugins.eslint())
     .pipe(plugins.eslint.format())
-    .pipe(plugins.eslint.failAfterError());
+    // .pipe(plugins.eslint.failAfterError());
 });
 
 gulp.task('mocha', function() {
   return gulp.src(paths.tests)
-    .pipe(plugins.mocha({
+    .pipe(plugins
+      .mocha({
       reporter : 'spec',
       bail : true
-    }));
+    })
+      .on("error", handleError));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.watch, ['test']);
+  gulp.watch(paths.watch, gulp.series('test'));
 });
 
 gulp.task('test', gulp.series('lint', 'mocha'));
 
-gulp.task('default', gulp.series('test', 'watch'));
+gulp.task('default', gulp.series(gulp.parallel('test', 'watch')));
