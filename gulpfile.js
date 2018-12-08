c75c9020c9fb8d9ff9f225f6f470748e43588986
Jocelyn Belden <jocebelden@gmail.com>
2018-12-08 11:58:51 -1000

    remove coveralls, plumber, and sourcemaps deps

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,6 @@ const paths = {
   tests : ['./test/**/*.js', '!test/{temp,temp/**}']
 };
 
-const plumberConf = {};
-
-if (process.env.CI) {
-  plumberConf.errorHandler = function(err) {
-    throw err;
-  };
-}
-
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
@@ -26,7 +18,7 @@ gulp.task('lint', function(done) {
   return gulp.src(paths.lint)
     .pipe(plugins.eslint())
     .pipe(plugins.eslint.format())
-    // .pipe(plugins.eslint.failAfterError());
+    .pipe(plugins.eslint.failAfterError());
 });
 
 gulp.task('mocha', function() {
